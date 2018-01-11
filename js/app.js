6fa44c71b4963b1b505893a85e61bcdcab934cb0
Munadil Fahad <munadil98@gmail.com>
2018-01-11 23:47:09 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,97 @@
+
+$(document).ready(function(){
+
+	$( function() {
+    	$( document ).tooltip();
+  	} );
+
+	$(".form").sortable();
+	// $("#at").text("Email can't be blank");
+	$("#email").keyup(function(){
+
+		//Email Validity
+		var noData = $("#email").val();
+		console.log(noData);
+		var at=$("#email").val().indexOf("@");
+		var com=$("#email").val().indexOf(".com");
+		
+		if (noData=="") {
+			$("#at").text("Email can't be blank");
+		}
+		else if(noData!=""&&(at<=-1||com<=-1)){
+			$("#at").text("Not a valid Email");
+		}
+		else{
+			$("#at").text("Email address is valid, thank you!");	
+		}
+
+	});
+
+	
+	// Full Name from first and last name
+
+	$("#lastName").keyup(function(){
+
+		firstName=$("#firstName").val();
+		lastName=$("#lastName").val();
+		var fullName=firstName.concat(" ").concat(lastName);
+		$("#fullName").val(fullName);
+
+	});
+
+	$("#firstName").keyup(function(){
+		firstName=$("#firstName").val();
+		lastName=$("#lastName").val();
+		
+		if (/\w/.test(firstName)) {
+    		$("#firstNameMessage").text("");
+		}
+		if (/\W/.test(firstName)) {
+	    	if (/\S/.test(firstName)) {
+	    		$("#firstNameMessage").text("This is not an alphabet");
+	    	}
+		}
+
+		if (/\d/.test(firstName)) {
+	    	$("#firstNameMessage").text("Number is not allowed");
+		}
+	})
+
+	$("#lastName").keyup(function(){
+		lastName=$("#lastName").val();
+		lastName=$("#lastName").val();
+		
+		if (/\w/.test(lastName)) {
+    		$("#lastNameMessage").text("");
+		}
+		if (/\W/.test(lastName)) {
+	    	if (/\S/.test(lastName)) {
+	    		$("#lastNameMessage").text("This is not an alphabet");
+	    	}
+		}
+
+		if (/\d/.test(lastName)) {
+	    	$("#lastNameMessage").text("Number is not allowed");
+		}
+	})
+
+
+$("#submit").click(function(){
+
+	$.ajax({
+		type: "POST",
+		url: "form_data.php",
+		data: firstName=firstName, 
+		success: function(result){
+			console.log("firstName: "+firstName);
+        $("#success").text("Form successfully submitted");
+    	},
+    	error: function(result){
+    		console.log("error");
+    	}
+    });
+
+})
+	
+
+});
